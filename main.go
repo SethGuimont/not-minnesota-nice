6afc56692aec8daa3518acfa7a81a866115f8d17
Seth Guimont <sethguimont@outlook.com>
2024-11-11 19:10:20 -0600

    Added Cors, figured out templates

--- main.go
+++ main.go
@@ -5,20 +5,31 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLFiles("views/index.html", "views/episodes.html")
 	router.Static("/views", "./views")
+	router.LoadHTMLGlob("views/*.tmpl")
+
+    config := cors.DefaultConfig()
+    router.Use(cors.New(config))
+
+
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Home",
+		})
 	})
 
 	router.GET("/episodes", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "episodes.html", nil)
+		c.HTML(http.StatusOK, "episodes.tmpl", gin.H{
+			"title": "Episodes",
+		})
 	})
 
 	router.GET("/stream/:filename", func(c *gin.Context) {
